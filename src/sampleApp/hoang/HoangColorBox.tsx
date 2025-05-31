a3367134dfaa9bfcc48acaf8c17cdbf8c415c220
tony nguyen <nhattruong1811@gmail.com>
2025-05-31 09:58:08 +0700

    ada

--- src/sampleApp/hoang/HoangColorBox.tsx
+++ src/sampleApp/hoang/HoangColorBox.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function HoangColorBox() {
-  return <div>Hoang ColorBox -- Hai updated</div>;
+  return <div>Hoang ColorBox -- Hai updated12321321</div>;
 }
 
 export default HoangColorBox;

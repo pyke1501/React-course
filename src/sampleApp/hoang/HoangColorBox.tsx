2a39ffa78d84840bc460a541861ca0f01bd62eae
Hai Tran <haith.contact@gmail.com>
2025-05-31 09:33:33 +0700

    update HoangColorBox 123

--- src/sampleApp/hoang/HoangColorBox.tsx
+++ src/sampleApp/hoang/HoangColorBox.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function HoangColorBox() {
-  return <div>Hoang ColorBox -- Hai updated</div>;
+  return <div>Hoang ColorBox -- Hai updated -- 123</div>;
 }
 
 export default HoangColorBox;
